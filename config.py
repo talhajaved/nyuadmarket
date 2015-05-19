4419df0f4fce6477c1e08441a25c13b20c5d5c22
talhajaved <talhajaved1994@gmail.com>
2015-05-19 22:54:45 +0400

    Updated config to add Admin functionalities

--- config.py
+++ config.py
@@ -1,33 +1,21 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
 
-OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
-    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
-    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
-    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SSL_DISABLE = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_RECORD_QUERIES = True
+    NYUAD_MARKET_ADMIN = os.environ.get('NYUAD_MARKET_ADMIN')
+    NYUAD_MARKET_POSTS_PER_PAGE = 20
+    NYUAD_MARKET_FOLLOWERS_PER_PAGE = 50
+    NYUAD_MARKET_COMMENTS_PER_PAGE = 30
+    NYUAD_MARKET_SLOW_DB_QUERY_TIME=0.5
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-WHOOSH_BASE = os.path.join(basedir, 'search.db')
-
-# mail server settings
-MAIL_SERVER = 'localhost'
-MAIL_PORT = 25
-MAIL_USERNAME = None
-MAIL_PASSWORD = None
-
-# administrator list
-ADMINS = ['you@example.com']
-
-# pagination
-POSTS_PER_PAGE = 3
-MAX_SEARCH_RESULTS = 50
-COMMENTS_PER_PAGE = 5
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class DevelopmentConfig(Config):
